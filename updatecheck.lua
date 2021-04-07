1d58f6d8ce6461150ab43460f76339b403f89d78
Patrick <14859495+Patrick2562@users.noreply.github.com>
2021-04-07 15:26:44 +0200

    [Modify] Update check

--- updatecheck.lua
+++ updatecheck.lua
@@ -1,7 +1,11 @@
--- SETTINGS -------------------------------------------------------------------------------------------
+-- SETTINGS - You can modify these variables ----------------------------------------------------------
 local CHECK_FOR_UPDATES = true -- change this to 'false' if you don't want to check for new releases!
 -------------------------------------------------------------------------------------------------------
 
+
+-- DON'T MODIFY THESE VARIABLES ---------------
+local VERSION = 113
+-----------------------------------------------
 
 if CHECK_FOR_UPDATES then
     addEventHandler("onResourceStart", resourceRoot, function()
@@ -11,46 +15,33 @@ if CHECK_FOR_UPDATES then
             data = fromJSON(data)
             
             if data then
-                local latest_id = tonumber(data["id"])
-                local tag_name  = tostring(data["tag_name"])
+                local tag_name       = tostring(data["tag_name"])
+                local latest_version = tonumber( (tag_name:gsub("v",""):gsub("%.","")) )
 
-                if latest_id then
-                    local file = fileExists("release") and fileOpen("release") or fileCreate("release")
-                    
-                    if file then
-                        local current_id = tonumber(fileRead(file, fileGetSize(file)))
+                if latest_version then
+                    if latest_version > VERSION then
+                        local asset = data["assets"][1]
 
-                        fileSetPos(file, 0)
+                        if asset then
+                            local path = "releases/"..asset["name"]
 
-                        if not current_id then
-                            fileWrite(file, tostring(latest_id))
-                        
-                        elseif latest_id > current_id then
-                            local asset = data["assets"][1]
+                            if fileExists(path) then
+                                print("[pAttach] New release ("..tag_name..") available on Github! It's already downloaded into 'releases' directory inside pAttach, just replace the old one!")
 
-                            if asset then
-                                local path = "releases/"..asset["name"]
+                            else
+                                fetchRemote(asset["browser_download_url"], function(data, status)
+                                    assert(status == 0 and data, "[pAttach] Can't download latest release ("..tag_name..") from Github! (Status code: "..tostring(status)..")")
+                                
+                                    local zip = fileCreate(path)
+                                    if zip then
+                                        fileWrite(zip, data)
+                                        fileClose(zip)
 
-                                if fileExists(path) then
-                                    print("[pAttach] New release ("..tag_name..") available on Github! It's already downloaded into 'releases' directory inside pAttach, just replace the old one!")
-
-                                else
-                                    fetchRemote(asset["browser_download_url"], function(data, status)
-                                        assert(status == 0 and data, "[pAttach] Can't download latest release ("..tag_name..") from Github! (Status code: "..tostring(status)..")")
-                                    
-                                        local zip = fileCreate(path)
-                                        if zip then
-                                            fileWrite(zip, data)
-                                            fileClose(zip)
-
-                                            print("[pAttach] New release ("..tag_name..") available on Github! Automatically downloaded into 'releases' directory inside pAttach, just replace the old one!")
-                                        end
-                                    end)
-                                end
+                                        print("[pAttach] New release ("..tag_name..") available on Github! Automatically downloaded into 'releases' directory inside pAttach, just replace the old one!")
+                                    end
+                                end)
                             end
                         end
-
-                        fileClose(file)
                     end
                 end
             end
